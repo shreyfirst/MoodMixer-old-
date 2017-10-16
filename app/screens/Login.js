3c94f0fa4943397110803860c1f1c0d5a855a3c6
Shrey Gupta <shreygupta@Shreys-MacBook-Pro-3.local>
2017-10-15 19:54:30 -0700

    fixed logo transparency

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, AppRegistry, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, AppRegistry, TextInput, Dimensions } from 'react-native';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: 'Useless Placeholder' };
+    this.state = { text: '' };
   }
   
 
-  render() {
+  render() { 
+    const {height, width}=Dimensions.get("window")
     return (
       <View 
         style={{
           flex: 1,
           alignItems: "center",
-          backgroundColor: "#0B0B0B"
+          backgroundColor: "#333333"
         }}
       >
         <View
         style = {{
-          flex: 0}} />
+          height: height * 0.1785714286}} />
         <Image
           source={require('../resources/MusicMixer.png')}
         />
         <Text
           style = {{ color: '#FFFFFF' }}
         > Login using Spotify</Text>
+        <TextInput
+        style={{height: 35, borderColor: 'gray', borderWidth: 1, color: 'white', width: 225, borderRadius: 5, textAlign: "center"}}
+        onChangeText={(text) => this.setState({text})}
+        placeholder="Username or email"
+        autoCapitalize="none"
+        placeholderTextColor="#EFEFEF"
+        value={this.state.text}
+        />
       </View>
     );
   }
